c99b895cb692d330d7a76c2054a64e58a0fa7a94
JARVIS\leonel <ibarra.h.leonel@gmail.com>
2017-01-27 19:36:16 -0300

    Carpeta de Plantillas + Estaticos + Bootstrap

--- Pedidos/Pedidos/settings.py
+++ Pedidos/Pedidos/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'Pedidos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'Plantillas')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,3 +107,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Estaticos')]
