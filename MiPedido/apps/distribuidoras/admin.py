e2c0277c3f6295a6e4789069202a606f6477c04d
joalafu <lafuentejoaquin@gmail.com>
2017-02-02 10:13:07 -0300

    Update admin.py

--- MiPedido/apps/distribuidoras/admin.py
+++ MiPedido/apps/distribuidoras/admin.py
@@ -1,17 +1,22 @@
 from django.contrib import admin
-from .models import Anuncio, Permiso_Distribuidora
+from .models import Anuncio, Permiso_Distribudora, Ruta
 
 
 class AnuncioAdmin(admin.ModelAdmin):
-    list_display = ('id', 'titulo', 'fecha_creacion', 'fecha_inicio', 'fecha_fin')
+    list_display = ('id', 'titulo', 'fecha_ini', 'fecha_end')
     search_fields = ['titulo']
-    raw_id_fields = ['id_distribuidora']
-
 
 class Permiso_DistribuidoraAdmin(admin.ModelAdmin):
     list_display = ("nombre", 'descripcion')
 
+class RutaAdmin(admin.ModelAdmin):
+    #id_distribudora
+    #nombre
+    #recorrido
+    #dia
+    list_display = ('id_distribudora', 'nombre', 'recorrido', 'dia' )
+    search_fields = ('id_distribudora', 'nombre', 'recorrido', 'dia' )
 
 admin.site.register(Anuncio, AnuncioAdmin)
-admin.site.register(Permiso_Distribuidora, Permiso_DistribuidoraAdmin)
-# Register your models here.
+admin.site.register(Permiso_Distribudora, Permiso_DistribuidoraAdmin)
+admin.site.register(Ruta, RutaAdmin)
