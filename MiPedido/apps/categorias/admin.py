c84b9ca43c533b71a19778d35903b7fab784d4c0
JARVIS\leonel <ibarra.h.leonel@gmail.com>
2017-02-08 20:04:30 -0300

    modificando categorias admin

--- MiPedido/apps/categorias/admin.py
+++ MiPedido/apps/categorias/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import Marca, Categoria
 
+
 class MarcaAdmin(admin.ModelAdmin):
 	list_display = ['nombre']
 	search_fields = ['nombre']
 	ordering = ['nombre']
+
 
 class CategoriaAdmin(admin.ModelAdmin):
 	list_display = ['nombre']
