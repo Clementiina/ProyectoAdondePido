bf96e6d391f6684bd87757ab8e484527ae1d6b2b
JARVIS\leonel <ibarra.h.leonel@gmail.com>
2017-03-04 12:07:17 -0300

    pngs

--- MiPedido/apps/distribuidoras/views.py
+++ MiPedido/apps/distribuidoras/views.py
@@ -9,10 +9,12 @@ from .models import Distribuidora, Usuario_Distribuidora, Anuncio
 class VistaDistribuidora(TemplateView):
     template_name = 'distribuidora.html'
 
-    def get_context_data(self, **kwargs):
-        contexto = super(VistaDistribuidora, self).get_context_data(**kwargs)
-        contexto['distribuidora'] = Distribuidora.objects.get(id=kwargs['pk'])
-        return contexto
+    def get(self, request):
+        uname = request.user.username
+        dist = request.GET['dist']
+        distribuidora = Distribuidora.objects.get(persona_cargo__username=uname, id=dist)
+        return render(request, 'distribuidora.html', {'dist': distribuidora})
+
 
 class FormatoFecha():
 
@@ -59,6 +61,24 @@ class VistaAnuncio(ListView):
     model = Anuncio
     template_name = 'anuncios.html'
     context_object_name = "anuncios"
+
+    def get(self, request):
+        uname = request.user.username
+        dist = request.GET['dist']
+        listado = Anuncio.objects.filter(id_distribuidora__persona_cargo__username=uname, id_distribuidora__id=dist)
+        paginador = Paginator(listado, 5)
+        pagina = request.GET['pag']
+        try:
+            anuncios = paginador.page(pagina)
+        except PageNotAnInteger:
+            anuncios = paginador.page(1)
+        except EmptyPage:
+            anuncios = paginador.page(paginador.num_pages)
+
+        contexto = {}
+        contexto['anuncios'] = anuncios
+        contexto['dist'] = dist
+        return render(request, 'anuncios.html', contexto)
 
 
 class CrearRuta(TemplateView):
