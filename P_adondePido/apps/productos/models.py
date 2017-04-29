ebf23827c5b3a8ffb4177fb744d09a730fcc341e
00kuX <martinylosdemas@gmail.com>
2017-04-29 19:02:59 -0300

    Cambios en el models productos

--- P_adondePido/apps/productos/models.py
+++ P_adondePido/apps/productos/models.py
@@ -5,12 +5,27 @@ class Presentacion (models.Model):
 	capacidad = models.CharField(max_length=50)
 	estado = models.BooleanField(default=True)
 
+	def __str__(self):
+		return self.capacidad
+
 
 class Producto (models.Model):
 	marcaSubCategoria = models.ForeignKey(Marca_SubCategoria)
 	nombre = models.CharField(max_length=50)
-	descripsiom = models.TextField()
+	descripcion = models.TextField()
+	estado = models.BooleanField(default=True)
+
+	def __str__(self):
+		return self.nombre
+
+class Producto_Presentacion(models.Model):
+	producto = models.ForeignKey(Producto)
 	presentacion = models.ForeignKey(Presentacion)
 	precio_unitario = models.FloatField()
 	estado = models.BooleanField(default=True)
 
+	class Meta:
+		unique_together = ('producto', 'presentacion')
+
+	def __str__(self):
+		return '%s en una presentacion de: %s ' % (self.producto, self.presentacion)
